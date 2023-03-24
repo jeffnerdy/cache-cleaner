c3bf8fe3d558e1f57f997f871708cefdc54b9e9c
jeffnerdy <110793443+jeffnerdy@users.noreply.github.com>
2023-03-24 19:45:18 +0000

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,18 +1,18 @@
 #pragma once
 
 #include <iostream>
+#include <filesystem>
 #include <string>
 #include <fstream>
-#include <filesystem>
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
-bool SetUp(std::string search, std::string clean);
+bool SetUp(const std::filesystem::path& search, const std::filesystem::path& clean);
 bool IsEmpty(std::ifstream& file);
 
-int SearchDirectory(const std::string& path, std::ofstream& file);
-int RemoveFromDirectory(const std::string& path);
+int SearchDirectory(const std::filesystem::path& path, std::ofstream& file);
+int RemoveFromDirectory(const std::filesystem::path& path);
 
 bool Search(std::string& path, std::ifstream& ifile, std::ofstream& ofile);
 bool Clean(std::string& path, std::ifstream& file);
